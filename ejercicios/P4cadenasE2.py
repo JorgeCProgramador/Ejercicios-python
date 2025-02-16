f62a92a65235ef502362547e8708ef7bc3d96948
JorgeCProgramador <jorgethebest31@gmail.com>
2025-02-16 00:21:20 +0000

    2.1commit

--- ejercicios/P4cadenasE2.py
+++ ejercicios/P4cadenasE2.py
@@ -1,5 +1,5 @@
 correo = input('Dame un correo: ')
 
 print('Longitud del correo: ', len(correo))
-print('Correo en mayusculas: ', correo.upper)
-print('Correo en minusculas: ', correo.lower)
+print('Correo en mayusculas: ', correo.upper())
+print('Correo en minusculas: ', correo.lower())
